e2be4233b68eb63352680aeabfd216095c14fa8a
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-11-24 00:28:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,22 @@
             head2.style.display = "none";
             return false;
         }
+
+        // -- curesol
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            autoplay:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        })
