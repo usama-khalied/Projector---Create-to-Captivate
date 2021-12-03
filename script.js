3a6578c4298cea8fc84328d9251270de74a6ce1a
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-03 23:56:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -40,3 +40,18 @@
                 }
             }
         })
+
+// -- swiper slider
+
+        var swiper = new Swiper(".mySwiper", {
+            pagination: {
+              el: ".swiper-pagination",
+              type: "progressbar",
+            },
+            navigation: {
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+              autoplay: true,
+
+            },
+          });
